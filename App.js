342a9cf927326aecc24e3bc3a85dcebb16abd0cc
Feyzi KAZIK <37486885+fevzikazik@users.noreply.github.com>
2022-10-29 22:10:21 +0300

    OnlineLearningApp init

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { LogBox } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+import {
+  MainLayout
+} from "./screens";
+
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import { CustomFonts } from './constants';
+
+const _loadAssets = async () => {
+  await Font.loadAsync(CustomFonts);
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  LogBox.ignoreAllLogs(true);
+
+  // React.useEffect(() => {
+  //   SplashScreen.hide();
+  // }, [])
+
+  const [isReady, setReady] = useState(false);
+
+  if (!isReady) {
+    // this is what makes sure the fonts are ready before loading the app
+    return (
+      <AppLoading
+        startAsync={_loadAssets} // this loads the fonts
+        onFinish={() => setReady(true)}
+        onError={e => console.error(e)}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName={'Dashboard'}
+      >
+        <Stack.Screen
+          name="Dashboard"
+          component={MainLayout}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
