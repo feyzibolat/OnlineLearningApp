de9a1be5715778553e7098214c8c7f2182aeb713
Feyzi KAZIK <37486885+fevzikazik@users.noreply.github.com>
2022-10-29 22:23:10 +0300

    Update MainLayout.js

--- screens/Dashboard/MainLayout.js
+++ screens/Dashboard/MainLayout.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import {
     View,
-    Text
+    Text,
+    TouchableOpacity,
+    Image,
+    Animated
 } from 'react-native';
 
+import {
+    Home,
+    Profile,
+    Search
+} from '../../screens';
+
+import { COLORS, SIZES, FONTS, constants } from '../../constants'
+
 const MainLayout = () => {
+
+    const flatListRef = React.useRef()
+
+    function renderContent() {
+        return (
+            <View
+                style={{
+                    flex: 1
+                }}
+            >
+                <Animated.FlatList
+                    ref={flatListRef}
+                />
+            </View>
+        )
+    }
+
     return (
-        <View>
-            <Text>MainLayout</Text>
+        <View
+            style={{
+                flex: 1,
+                backgroundColor: COLORS.white
+            }}
+        >
+            {/* Content */}
+            {renderContent()}
+
+            {/* Bottom Tab */}
+
         </View>
     )
 }
